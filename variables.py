89a21004f5c91e7c317b69decee04a1e06046a14
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-01-20 01:35:36 +0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,34 @@
+//
+x = 5
+y = "John"
+print(x)
+print(y)
+
+//
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+//
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+//
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+//
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+//
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
