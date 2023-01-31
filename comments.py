c5423b16c8edc8d92e541e7b0b2226c04540d48f
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-02-01 01:14:11 +0600

    Add files via upload
    tsis1

--- comments.py
+++ comments.py
@@ -1,23 +1,2 @@
 #This is a comment
 print("Hello, World!")
-
-//
-print("Hello, World!") #This is a comment
-
-//
-#print("Hello, World!")
-print("Cheers, Mate!")
-
-//
-#This is a comment
-#written in
-#more than just one line
-print("Hello, World!")
-
-//
-"""
-This is a comment
-written in 
-more than just one line
-"""
-print("Hello, World!")
