e35d24021f730b6a5cd6a9e2c853b2d613064cce
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-02-01 01:40:45 +0600

    Create for loops.py

---
+++ tsis2/for loops.py
@@ -0,0 +1,15 @@
+for x in "banana":
+  print(x)
+  
+  //
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+    
+    //
+    for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
