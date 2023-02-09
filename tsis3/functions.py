e0470dcdd3c5704b8c5fd261ac72c6624e20214c
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-02-09 15:09:00 +0600

    Create functions.py

---
+++ tsis3/functions.py
@@ -0,0 +1,12 @@
+ef my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+#
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
