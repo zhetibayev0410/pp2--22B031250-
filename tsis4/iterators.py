6d4fd0b98e094447cc7e6f3be631c48589d7570d
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-02-22 22:04:03 +0600

    Create iterators.py

---
+++ tsis4/iterators.py
@@ -0,0 +1,19 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+//
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
