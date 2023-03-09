baa0208f7c4449a93fbc1b99b7e7266a290a2ec9
Yersultan <123096930+zhetibayev0410@users.noreply.github.com>
2023-03-09 14:23:43 +0600

    Update 1st.py

--- tsis6/1st.py
+++ tsis6/1st.py
@@ -1,1 +1,11 @@
+#Write a Python program with builtin function to multiply all the numbers in a list
+from functools import reduce
+
+def multiply_list(numbers):
+    return reduce((lambda x, y: x * y), numbers)
+
+my_list = [1, 2, 3, 4, 5]
+result = multiply_list(my_list)
+
+print("The product of all the numbers in the list is:", result)
 
